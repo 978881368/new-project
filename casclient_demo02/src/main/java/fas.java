30caf96e03da42f61751e8a0edb88bd42a9eb5eb
csy978881368 <978881368@qq.com>
2019-04-12 12:11:03 +0800

    第二次提交

---
+++ casclient_demo02/src/main/java/fas.java
@@ -0,0 +1,14 @@
+import javax.servlet.ServletOutputStream;
+
+/**
+ * @ClassName fas
+ * @Descriptions 范德萨
+ * @Author BERNARD
+ * @Date 12:09 2019/4/12
+ * @Version 2.2
+ */
+public class fas {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
