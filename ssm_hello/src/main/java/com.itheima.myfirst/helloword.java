12919caf8648a99ee65d383f1da51bc6d3908fe3
csy978881368 <978881368@qq.com>
2019-04-12 12:21:46 +0800

    第一次提交gitTest

--- ssm_hello/src/main/java/com.itheima.myfirst/helloword.java
+++ ssm_hello/src/main/java/com.itheima.myfirst/helloword.java
@@ -3,5 +3,6 @@ package com.itheima.myfirst;
 public class helloword {
     public static void main(String[] args) {
         System.out.println("helloword!!!!");
+        System.out.println("fuck you");
     }
 }
