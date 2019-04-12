5bd68198590bf600c2779ebc09e4871090601d62
csy978881368 <978881368@qq.com>
2019-04-12 12:11:55 +0800

    第二次提交

---
+++ casclient_demo01/src/main/java/java.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName java
+ * @Descriptions hellorword
+ * @Author BERNARD
+ * @Date 12:11 2019/4/12
+ * @Version 2.2
+ */
+public class java {
+    public static void main(String[] args) {
+        System.out.println("hellorword");
+    }
+}
